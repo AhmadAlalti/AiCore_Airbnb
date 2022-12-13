4aab3afbe04d3e849af9654fc3531b9cd10da70d
Ahmad <110533174+alalti@users.noreply.github.com>
2022-12-13 16:53:48 +0000

    Renames variable properly.

--- hyperparams_config_file.py
+++ hyperparams_config_file.py
@@ -1,7 +1,7 @@
 #This configuration file contains the dictionairies of different hyperparameters for different models
 
 #Regression models hyperparameters
-sgd_param = {'loss': ['squared_error', 'huber', 'epsilon_insensitive','squared_epsilon_insensitive'],
+sgd_params = {'loss': ['squared_error', 'huber', 'epsilon_insensitive','squared_epsilon_insensitive'],
              'penalty' : ['l2', 'l1', 'elasticnet'],
              'alpha': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100],
              'max_iter' : [750, 1000, 1250, 1500],
