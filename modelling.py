6b5b693f70f25e9c5f89914caedffd1176f78815
Ahmad <110533174+alalti@users.noreply.github.com>
2022-12-27 10:28:42 +0000

    Evaluates different regression models.

--- modelling.py
+++ modelling.py
@@ -6,6 +6,8 @@ import joblib
 import json
 import os
 from tabular_data import load_airbnb
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -15,35 +17,19 @@ def split_data(X, y):
     X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=1) #Is the test size correct? original 30%, split into two 15%
     return X_train, y_train, X_test, y_test, X_val, y_val
 
+def evaluate_all_models(models_and_params, data_sets):
+    for model in models_and_params.items():
+        RMSE, MAE, R2, best_iteration_hyperparams, gs_reg = tune_regression_model_hyperparameters(model[0], data_sets, model[1])
+        regression_metrics = {"RMSE": RMSE, "MAE": MAE, "R2": R2}
+        path = f"models/regression/{model[0].__name__}/"
+        save_model(gs_reg, best_iteration_hyperparams, regression_metrics, path )
+
 def regression_scoring(X, y, model):
     y_pred = model.predict(X)
     RMSE = mean_squared_error(y, y_pred, squared=False)
     MAE = mean_absolute_error(y, y_pred)
     R2 = r2_score(y, y_pred)
     return RMSE, MAE, R2
-
-def custom_tune_regression_model_hyperparameters(model_type, data_sets, grid_dict):
-    keys, values = zip(*grid_dict.items())
-    iterations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
-    RMSE_list = []
-    for iteration in iterations_dicts:
-        loss = iteration["loss"]
-        penalty = iteration["penalty"]
-        alpha = iteration["alpha"]
-        max_iter = iteration["max_iter"]
-        learning_rate = iteration["learning_rate"]
-        model = model_type(loss=loss, penalty=penalty, alpha=alpha, max_iter=max_iter, learning_rate=learning_rate)
-        model.fit(data_sets[0], data_sets[1])
-        # RMSE_Train, MAE_Train, R2_Train = regression_scoring(data_sets[0], data_sets[1], model)
-        RMSE_Val, MAE_Val, R2_Val = regression_scoring(data_sets[4], data_sets[5], model)
-        RMSE_list.append(RMSE_Val)
-        if RMSE_Val <= min(RMSE_list):
-            RMSE = RMSE_Val
-            MAE = MAE_Val
-            R2 = R2_Val
-            best_iteration_hyperparams = iteration
-            best_model = model
-    return RMSE, MAE, R2, best_iteration_hyperparams, best_model
 
 def tune_regression_model_hyperparameters(model_type, data_sets, grid_dict):
     model = model_type()
@@ -75,12 +61,38 @@ if __name__ == "__main__":
     X = X.select_dtypes(include="number")
     X_train, y_train, X_test, y_test, X_val, y_val = split_data(X, y)
     data_sets = [X_train, y_train, X_test, y_test, X_val, y_val]
-    reg = SGDRegressor
+    models_and_params = {SGDRegressor: hp.SGDRegressor, 
+                         DecisionTreeRegressor: hp.DecisionTreeRegressor, 
+                         RandomForestRegressor: hp.RandomForestRegressor, 
+                         GradientBoostingRegressor: hp.GradientBoostingRegressor}
+    evaluate_all_models(models_and_params, data_sets)
+    
     # RMSE, MAE, R2, best_iteration_hyperparams, best_model = custom_tune_regression_model_hyperparameters(reg, data_sets, hp.sgd_params)
     # print(f"Best model: {best_model} \nBest Hyperparameters: {best_iteration_hyperparams} \nRMSE = {RMSE} \nMAE = {MAE} \nR2 = {R2}")
-    RMSE, MAE, R2, best_iteration_hyperparams, gs_reg = custom_tune_regression_model_hyperparameters(reg, data_sets, hp.sgd_params)
-    print(f"Best model: {gs_reg} \nBest Hyperparameters: {best_iteration_hyperparams} \nRMSE = {RMSE} \nMAE = {MAE} \nR2 = {R2}")
-    regression_metrics = {"RMSE": RMSE, "MAE": MAE, "R2": R2}
-    save_model(gs_reg, best_iteration_hyperparams, regression_metrics, "models/regression/linear_regression/")
+    # print(f"Best model: {gs_reg} \nBest Hyperparameters: {best_iteration_hyperparams} \nRMSE = {RMSE} \nMAE = {MAE} \nR2 = {R2}")
+    # regression_metrics = {"RMSE": RMSE, "MAE": MAE, "R2": R2}
+    # save_model(gs_reg, best_iteration_hyperparams, regression_metrics, "models/regression/linear_regression/")
     
-    
+
+# def custom_tune_regression_model_hyperparameters(model_type, data_sets, grid_dict):
+#     keys, values = zip(*grid_dict.items())
+#     iterations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
+#     RMSE_list = []
+#     for iteration in iterations_dicts:
+#         loss = iteration["loss"]
+#         penalty = iteration["penalty"]
+#         alpha = iteration["alpha"]
+#         max_iter = iteration["max_iter"]
+#         learning_rate = iteration["learning_rate"]
+#         model = model_type(loss=loss, penalty=penalty, alpha=alpha, max_iter=max_iter, learning_rate=learning_rate)
+#         model.fit(data_sets[0], data_sets[1])
+#         # RMSE_Train, MAE_Train, R2_Train = regression_scoring(data_sets[0], data_sets[1], model)
+#         RMSE_Val, MAE_Val, R2_Val = regression_scoring(data_sets[4], data_sets[5], model)
+#         RMSE_list.append(RMSE_Val)
+#         if RMSE_Val <= min(RMSE_list):
+#             RMSE = RMSE_Val
+#             MAE = MAE_Val
+#             R2 = R2_Val
+#             best_iteration_hyperparams = iteration
+#             best_model = model
+#     return RMSE, MAE, R2, best_iteration_hyperparams, best_model
