13eda509995fa355d4d2924c4abd47e5dedf226a
Ahmad <110533174+alalti@users.noreply.github.com>
2023-02-02 16:38:26 +0000

    Implements a preliminary NN.

--- neural_network.py
+++ neural_network.py
@@ -9,21 +9,20 @@ class AirbnbNightlyPriceImageDataset(Dataset):
         super().__init__()
         clean_data = pd.read_csv('airbnb-property-listings/tabular_data/clean_tabular_data.csv')
         self.features, self.label = load_airbnb(clean_data, 'Price_Night')
-        self.features = self.features.select_dtypes(include=["int64", "float64"])        
-        
+        self.features = self.features.select_dtypes(include=["int64", "float64"])
     
     def __getitem__(self, index):
         features = self.features.iloc[index]
         features = torch.tensor(features)
-        label = self.label.iloc[index]        
+        label = self.label.iloc[index] 
         return features, label
 
     def __len__(self):
         return len(self.features)
 
 def get_random_split(dataset):
-    train_set, test_set = random_split(dataset, [70, 30])
-    train_set, validation_set = random_split(train_set, [50, 50])
+    train_set, test_set = random_split(dataset, [0.7, 0.3])
+    train_set, validation_set = random_split(train_set, [0.5, 0.5])
     return train_set, validation_set, test_set
     
 def get_data_loader(dataset, batch_size=16):
@@ -33,10 +32,27 @@ def get_data_loader(dataset, batch_size=16):
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
     return train_loader, validation_loader, test_loader
 
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_layer = torch.nn.Linear(11, 1)
+    
+    def forward(self, features):
+        return self.linear_layer(features)
+    
+def train(model, data_loader, epochs):
+    for epoch in range(epochs):
+        for batch in data_loader:
+            features, label = batch
+            features = features.to(torch.float32)
+            prediction = model(features)
+            print(prediction)
 
 if __name__ == '__main__':
     np.random.seed(1)
     dataset = AirbnbNightlyPriceImageDataset()
     train_loader, validation_loader, test_loader = get_data_loader(dataset)
+    model = NeuralNetwork()
+    train(model, train_loader, 1)
     
     
