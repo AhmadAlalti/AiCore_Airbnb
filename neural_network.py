13d8654387b006c192803696c1c707b7ab5d61e4
Ahmad <110533174+alalti@users.noreply.github.com>
2023-02-03 23:04:33 +0000

    Implements tensor board.

--- neural_network.py
+++ neural_network.py
@@ -1,8 +1,11 @@
 from tabular_data import load_airbnb
 from torch.utils.data import Dataset, DataLoader, random_split 
+import torch.nn.functional as F
 import torch
 import numpy as np
 import pandas as pd
+from torch.utils.tensorboard import SummaryWriter
+torch.manual_seed(2)
 
 class AirbnbNightlyPriceImageDataset(Dataset):
     def __init__(self):
@@ -13,8 +16,9 @@ class AirbnbNightlyPriceImageDataset(Dataset):
     
     def __getitem__(self, index):
         features = self.features.iloc[index]
-        features = torch.tensor(features)
-        label = self.label.iloc[index] 
+        features = torch.tensor(features).float()
+        label = self.label.iloc[index]
+        label = torch.tensor(label).float()
         return features, label
 
     def __len__(self):
@@ -25,7 +29,7 @@ def get_random_split(dataset):
     train_set, validation_set = random_split(train_set, [0.5, 0.5])
     return train_set, validation_set, test_set
     
-def get_data_loader(dataset, batch_size=16):
+def get_data_loader(dataset, batch_size=32):
     train_set, validation_set, test_set = get_random_split(dataset)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True)
@@ -40,19 +44,29 @@ class NeuralNetwork(torch.nn.Module):
     def forward(self, features):
         return self.linear_layer(features)
     
-def train(model, data_loader, epochs):
+def train(model, data_loader, epochs=15):
+    
+    optimiser = torch.optim.SGD(model.parameters(), lr=0.0001)
+    writer = SummaryWriter()
+    batch_idx = 0
     for epoch in range(epochs):
         for batch in data_loader:
             features, label = batch
-            features = features.to(torch.float32)
+            label = torch.unsqueeze(label, 1)
             prediction = model(features)
-            print(prediction)
+            loss = F.mse_loss(prediction, label)
+            loss.backward()
+            optimiser.step()
+            optimiser.zero_grad()
+            writer.add_scalar('loss', loss.item(), batch_idx)
+            batch_idx += 1
+            
 
 if __name__ == '__main__':
     np.random.seed(1)
     dataset = AirbnbNightlyPriceImageDataset()
     train_loader, validation_loader, test_loader = get_data_loader(dataset)
     model = NeuralNetwork()
-    train(model, train_loader, 1)
+    train(model, train_loader)
     
     
